db2deac6de7ddcbd62a986e1ba6d119e5b87bcda
Damian Jóźwiak <damian.jozwiak@venturedevs.com>
2018-10-20 15:54:19 +0200

    Cleanup

--- src/store/index.js
+++ src/store/index.js
@@ -5,26 +5,6 @@ import createLogger from 'vuex/dist/logger';
 
 Vue.use(Vuex);
 
-// const store = {
-//   state: {
-//     products: [],
-//   },
-//   async fetchProducts(query = '') {
-//     ({ data: this.state.products } = await axios.get(`http://localhost:3000/products?q=${encodeURIComponent(query)}`));
-//   },
-//   addProduct(product) {
-//     this.state.products.push(product);
-//     return axios.post('http://localhost:3000/products', product);
-//   },
-//   updateProduct(id, name) {
-//     axios.put(`http://localhost:3000/products/${encodeURIComponent(id)}`, {
-//       name
-//     });
-//   }
-// };
-//
-// export default store;
-
 export default new Vuex.Store({
   state: {
     products: []
